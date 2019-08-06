8b600da2f0aa25fc49d74aa416c998eebd4d585f
Gérôme Grignon <Jerome.grignon@intra.bodet-timesport.com>
2019-08-06 16:56:50 +0200

    dev branch creation

--- example/index.js
+++ example/index.js
@@ -2,14 +2,6 @@ let bluetoothLEMidi = '03b80e5a-ede8-4b33-a751-6ce34ec4c700';
 let bleMidiCharacteristic = '7772e5db-3868-4112-a1a9-f2669d106bf3';
 let numSensors = 0;
 
-var cowbell = new Howl({src: ['../assets/cowbell.wav']});
-var kick = new Howl({src: ['../assets/kick.wav']});
-var snare = new Howl({src: ['../assets/snare.wav']});
-var closedHiHat = new Howl({src: ['../assets/closed-hi-hat.wav']});
-var bassDrum = new Howl({src: ['../assets/bass-drum.wav']});
-var hiHatCymbal = new Howl({src: ['../assets/hi-hat-cymbal.mp3']});
-var heavySnare = new Howl({src: ['../assets/heavy-snare.wav']});
-
 class FreedrumStick {
   constructor(name) {
     this.device = null;
@@ -37,6 +29,7 @@ class FreedrumStick {
       return Promise.reject('Device is not connected.');
     }
     numSensors+=1;
+    console.log(numSensors);
     return this.device.gatt.connect();
   }
   
@@ -64,81 +57,110 @@ class FreedrumStick {
       let data = event.target.value;
       let command = data.getUint8(2);
       let note = data.getUint8(3);
-      let volume = data.getUint8(4);
 
-      // The volume property in Howler.js is normalize (between 0 - 1)
-      // The velocity value in MIDI ranges from 0 - 127
-      // To normalize, here's the equation:
-      // z = (x - min(x)) / (max(x) - min(x))
-      // z = (volume - Math.min(0,127)) / (Math.max(0,127) - Math.min(0,127));
-      let normalizedVolume = (volume - Math.min(0,127)) / (Math.max(0,127) - Math.min(0,127));
+      console.log(this.device.id);
   
-      if(this.device.id === "XrnS1FRG/q/kM7ecsfErcg==" || this.device.id === "T/P6X0jDSadbdUXxRjjAVw=="){
-        this.handleDrumSticksEvents(command, note, normalizedVolume);
+      if(stickSensors.map(sensor => sensor.device.id).find(id => id === this.device.id) !== undefined){
+        if(this.device.id === stickSensors[0].device.id) {
+          document.getElementById('stick-sensor-signal-0').style.visibility = 'visible' ;
+        setTimeout(() => document.getElementById('stick-sensor-signal-0').style.visibility = 'hidden', 2000);
+        }
+
+        if(this.device.id === stickSensors[1].device.id) {
+          document.getElementById('stick-sensor-signal-1').style.visibility = 'visible' ;
+        setTimeout(() => document.getElementById('stick-sensor-signal-1').style.visibility = 'hidden', 2000);
+        }
+        
+        this.handleDrumSticksEvents(command, note);
       } 
       
-      if(this.device.id === "6lASiqGNnfck4kO66nRlGw==" || this.device.id === "dJTKMPg47ZLgP4PAEBuWZw=="){
-        this.handlePedalEvents(command, note, normalizedVolume);
+      if(this.device.id === "KK/SSgLwFlHSjxZqWDjzbA==" || this.device.id === "Dqr4hgehCfi7hpaxdic6eg=="){
+        this.handlePedalEvents(command, note);
       } 
   }
 
-  handleDrumSticksEvents(command, note, volume){
-    if(command === 153){
-      if(note === 50){
-        hiHatCymbal.play()
-        hiHatCymbal.volume(volume);
+  handleDrumSticksEvents(command, note){
+    console.log(command);
+    console.log(note);
+    if(command === 137){
+      if(note === 38){
         drawSquare();
       }
 
-      if(note === 51){
-        heavySnare.play()
-        heavySnare.volume(volume);
+      if(note === 57){
         drawCircle();
+      }
+
+      if(note === 41){
+        drawLine();
+      }
+
+      if(note === 46){
+        drawTriangle();
       }
     }
   }
 
-  handlePedalEvents(command, note, volume){
+  handlePedalEvents(command, note){
+    console.log(command);
+    console.log(note);
     if(command === 153){
-      if(note === 44){
-        bassDrum.play()
-        bassDrum.volume(volume);
+      if(note === 38){
+        // bassDrum.play()
+        // bassDrum.volume(volume);
         drawLine();
       }
 
       if(note ===50){
-        closedHiHat.play()
-        closedHiHat.volume(volume);
+        // closedHiHat.play()
+        // closedHiHat.volume(volume);
         drawTriangle();
       }
     }
   }
 }
+
+/* Define FreedrumStick and store them in 2 distincs arrays (for sticks and foot) */
   
-var freeDrumStickOne = new FreedrumStick("XrnS1FRG/q/kM7ecsfErcg=="); // FD2 v8
-var freeDrumStickTwo = new FreedrumStick("T/P6X0jDSadbdUXxRjjAVw=="); // FD2 v8
-var freeDrumFootOne = new FreedrumStick("6lASiqGNnfck4kO66nRlGw=="); // FD2 v9
-var freeDrumFootTwo = new FreedrumStick("dJTKMPg47ZLgP4PAEBuWZw=="); // FD2 v9
+var freeDrumStickOne = new FreedrumStick("aid8ioeA+Fvn2c8E66mZXQ=="); // FD2 v8
+var freeDrumStickTwo = new FreedrumStick("AZWyVIxNAtV/MDKcJ3VxWw=="); // FD2 v8
 
-const sensors = [freeDrumStickOne, freeDrumStickTwo, freeDrumFootOne, freeDrumFootTwo];
 
-document.querySelector('button').addEventListener('click', event => {
-  let currentSensor = getSensorToConnect()
+var freeDrumFootOne = new FreedrumStick("KK/SSgLwFlHSjxZqWDjzbA=="); // FD2 v9
+var freeDrumFootTwo = new FreedrumStick("Dqr4hgehCfi7hpaxdic6eg=="); // FD2 v9
+
+const stickSensors = [freeDrumStickOne, freeDrumStickTwo];
+const footSensors = [freeDrumFootOne, freeDrumFootTwo];
+
+/* Connects to bluetooth devices */
+
+document.querySelector('#stick-sensor-add-0').addEventListener('click', event => {
+  let currentSensor = getSensorToConnect(stickSensors)
   currentSensor.request()
   .then(_ => currentSensor.connect())
   .then(_ => { 
-    if(numSensors === 4){
-      const button = document.getElementsByTagName('button')[0];
-      button.classList.add('fade');
-      const title = document.getElementsByTagName('main')[0];
-      title.classList.add('fade');
-    }
-    currentSensor.getFreedrumData()
+    currentSensor.getFreedrumData();
+    console.log(currentSensor);
+    document.getElementById('stick-sensor-container-0').style.display = 'flex';
+    document.getElementById('stick-sensor-add-0').style.display = 'none';
   })
   .catch(error => { console.log(error) });
 });
 
-const getSensorToConnect = () => {
+document.querySelector('#stick-sensor-add-1').addEventListener('click', event => {
+  let currentSensor = getSensorToConnect(stickSensors)
+  currentSensor.request()
+  .then(_ => currentSensor.connect())
+  .then(_ => { 
+    currentSensor.getFreedrumData();
+    console.log(currentSensor);
+    document.getElementById('stick-sensor-container-1').style.display = 'flex';
+    document.getElementById('stick-sensor-add-1').style.display = 'none';
+  })
+  .catch(error => { console.log(error) });
+});
+
+const getSensorToConnect = (sensors) => {
   for(sensor of sensors){
     if(sensor.device === null){
       let currentSensor = sensor;
@@ -146,3 +168,23 @@ const getSensorToConnect = () => {
     }
   }
 }
+
+document.querySelector('#play-btn').addEventListener('click', event => {
+    const button = document.getElementsByTagName('button')[1];
+    button.classList.add('fade');
+    const title = document.getElementsByTagName('main')[0];
+    title.classList.add('fade');
+});
+
+
+document.getElementById('stick-sensor-btn-0').addEventListener('click', event => {
+  if(stickSensors[0].device !== null) {
+    stickSensors[0].disconnect();
+    stickSensors[0].device = null;
+  }
+});
+
+document.getElementById('stick-sensor-container-0').style.display = 'none';
+document.getElementById('stick-sensor-container-1').style.display = 'none';
+document.getElementById('foot-sensor-container-0').style.display = 'none';
+document.getElementById('foot-sensor-container-1').style.display = 'none';
